226f1e6e5e2bbac549f55dba025072ec7e6dd793
estenioalves <estenioalves94@gmail.com>
2023-05-30 20:11:56 -0300

    Criando metodo de validar CNPJ

---
+++ cnpj.py
@@ -0,0 +1,22 @@
+from validate_docbr import CNPJ
+class cnpj:
+    def __init__(self, documento):
+        documento = str(documento)
+        if self.cnpj_e_valido(documento):
+            self.cnpj = documento
+        else:
+            raise ValueError("CNPJ inválido!")
+
+    def cnpj_e_valido(self, cnpj):
+        if len(cnpj) == 14:
+            validador = CNPJ()
+            return validador.validate(cnpj)
+        else:
+            raise ValueError("Quantidade de dígitos inválida!")
+
+    def format_cnpj(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cnpj)
+
+    def __str__(self):
+        return self.format_cnpj()
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from cpf import cpf
+from cnpj import cnpj
 
 cpf_um = cpf("")
 print(cpf_um)
+
+cnpj_um = cnpj("")
+print(cnpj_um)
